54d5eb2a21a67805b97fb63ef0d005c2c54026bb
jtorbett23 <josh.torbett@outlook.com>
2025-08-07 17:36:13 +0100

    tweaks

--- gh-pages.sh
+++ gh-pages.sh
@@ -2,3 +2,12 @@
 git checkout master
 git branch -D gh-pages
 git checkout -b gh-pages
+rm -r ./*.py
+rm -r ./*.h5
+rm -r ./*.md
+rm -r ./*.lock
+rm -r ./Pipfile
+rm -r ./Dockerfile
+mv ui/* ./
+rm -r ./ui
+
