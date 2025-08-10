4d5d5720afed083665f49cfa50ba911ae3ab050a
jtorbett23 <josh.torbett@outlook.com>
2025-08-10 19:16:22 +0100

    rename files

--- api.py
+++ api.py
@@ -1,33 +1,36 @@
 from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 from use_model import predict_number
 import numpy
 import json
-#TODO
-# Only allow requests from the ui
-# Try saving the screenshot to a local python file and have the app read from that instead of passing the content as an api request
-# find a way to make sure tensorflow is setup prior to server
-# lock dependencies 
-# fix showing as it is not showing calcutating on render consistently
-# current deploy delay on render is 6mins can we speed this up
-# update the ui and have controls on it
+
 app = FastAPI(title="main app")
+
+origins = [
+    "https://jtorbett23.github.io"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+async def status():
+    return {"Status": "UP"}
 
 @app.post("/predict")
 async def predict(request: Request):
     try:
         image_data = await request.json()
-        print(type(image_data))
         image_data = json.loads(image_data)
-        print(type(image_data))
         image_data = numpy.array(image_data, dtype=numpy.float16)
-        print(type(image_data))
         prediction = predict_number(image_data)
-        print(prediction)
         prediction = int(prediction)
-        print(type(prediction))
     except:
         print("error")
     return prediction
-
-app.mount("/", StaticFiles(directory="ui", html=True), name="ui")
