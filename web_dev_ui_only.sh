efc757094866aa715d800bc501ec8ea421f8e4f6
jtorbett23 <josh.torbett@outlook.com>
2025-08-11 15:29:12 +0100

    run only ui script

---
+++ web_dev_ui_only.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+source .env || echo "no .env file"
+cp index.html ./ui/index.html
+cp pygame_core.py ./ui/pygame_core.py
+cp pygame_ui.py ./ui/pygame_ui.py
+sed -i "s|!URL|$URL|g" ./ui/pygame_ui.py
+sed -i "s|!URL|$URL|g" ./ui/index.html
+python -m fastapi dev number_reader_ui_only.py --port 8080
