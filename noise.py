1f7ec18766bdcaf2a6b4eee70b8d14b71a5d688c
jtorbett23 <josh.torbett@outlook.com>
2025-08-27 18:56:49 +0100

    remove testing code / scripts

--- noise.py
+++ noise.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 import cv2 as cv
 import numpy as np
 from skimage.util import random_noise
-from skimage.transform import SimilarityTransform, warp, resize
+from skimage.transform import SimilarityTransform, warp
 import math
 import random
 
@@ -30,36 +30,10 @@ def apply_noise(images):
         rotation_rads = math.radians(rotation_degs)
         scale = get_random_float(MIN_SCALE, MAX_SCALE)
 
-        # print(noise)
-        # print(offset_x)
-        # print(offset_y)
-        # print(rotation_degs)
-        # print(scale)
-
         tform = SimilarityTransform(scale=scale, rotation=math.radians(rotation_rads), translation=(offset_x, offset_y))
         image = warp(image, tform)
         image = random_noise(image, mode='gaussian', mean=0, var=noise)
         noisy_images.append(image)
     
-    print("Noise applied")
     return noisy_images
 
-def test():
-    mnist = tf.keras.datasets.mnist
-
-    (x_train, y_train), (x_test, y_test) = mnist.load_data()
-
-
-    first_image = x_train[0]
-    noisy_images = apply_noise([first_image])
-    # noisy_images_first = noisy_images[0] * 255
-
-    # cv.imwrite("images/mnist-noisy-2.png", noisy_images_first)
-
-    cv.imshow("wow",resize(first_image, (360,360)))
-    cv.waitKey(0)
-
-    cv.imshow("wow",resize(noisy_images[0], (360,360)))
-    cv.waitKey(0)
-
-# test()
--- use_model.py
+++ use_model.py
@@ -1,45 +1,12 @@
 import tensorflow as tf
-# import cv2 as cv
 import numpy as np
-# import imutils
 from skimage import io
 from skimage.transform import resize
 
-# model = tf.keras.models.load_model('numberreader.h5')
-
-
-# def prepare_image_cv(img_path):
-#     img = cv.imread(img_path)
-#     # cv.imwrite("images/original.png",img)
-#     img = imutils.resize(img, width=28)
-#     return img
-
 def prepare_image_sk(img_path):
     img = io.imread(img_path)
-    # cv.imwrite("images/original.png",img)
     img = resize(img, (28,28),anti_aliasing=True)
     return img
-
-# def predict_number_with_path(img_path):
-#     img = prepare_image_sk(img_path)
-    
-#     non_black_pixels = np.where(
-#         (img[:, :, 0] > (15/255)) 
-#     )
-
-#     # set all non black pixels to white
-#     human_image = img
-#     human_image[non_black_pixels] = (human_image[non_black_pixels] * 255) + 75
-
-#     img[non_black_pixels] = img[non_black_pixels] + (75/255)
-
-#     # cv.imwrite("images/resize.png",img)
-#     img = img[:,:,0]
-#     img = np.array([img])
-    
-#     predicition = model.predict(img)
-#     print(predicition)
-#     print(f"The digit is probably {np.argmax(predicition)}")
 
 def predict_number(img):
     model = tf.keras.models.load_model('numberreader.h5')
@@ -49,14 +16,11 @@ def predict_number(img):
         (img[:, :, 0] < 230) 
     )
     img[non_white_pixels] = 0
-    # cv.imwrite("images/original.png",img)
 
-    # if i need to use just numpy try - img = img[::12, ::12]
 
     img = resize(img, (28,28),anti_aliasing=True)
     
     # get (i, j) positions of all RGB pixels that are black (i.e. [0, 0, 0])
-
     non_black_pixels = np.where(
         (img[:, :, 0] > (15/255)) 
     )
@@ -67,7 +31,6 @@ def predict_number(img):
 
     img[non_black_pixels] = img[non_black_pixels] + (75/255)
 
-    # cv.imwrite("images/resize.png",human_image)
     img = img[:,:,0]
     img = np.array([img])
     
