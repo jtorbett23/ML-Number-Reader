9a79cbce40f1d39c2da8be6dde65500798bbdecc
jtorbett23 <josh.torbett@outlook.com>
2025-09-01 10:07:43 +0100

    revert testing changes

--- pygame_ui_sync.py
+++ pygame_ui_sync.py
@@ -43,7 +43,7 @@ def set_text(screen, content, current_text=None):
     return current_text
 
 
-text = set_text(screen, "Prediction: None - Test")
+text = set_text(screen, "Prediction: None")
 clear_button = Button("Clear", 0 + 10, height - 30 - 10, 80, 30, font)
 predict_button = Button("Predict", width -80 - 10, height - 30 - 10, 80, 30, font)
 clear_button.draw(screen)
@@ -98,7 +98,6 @@ def run_frame(is_local = False):
                 screenshot = None
                 retry_count = 0
                 print("Request finished", flush=True)
-
 
     for event in pygame.event.get():
         #MOUSE MOVED
