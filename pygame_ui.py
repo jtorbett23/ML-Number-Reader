bbe4aa8ef0d8a59563dc71be1339135abff53bbf
jtorbett23 <josh.torbett@outlook.com>
2025-09-01 02:53:32 +0100

    just sync

--- pygame_ui.py
+++ pygame_ui.py
@@ -1,11 +1,12 @@
-from pyscript import window
+# from pyscript import window
+from pygame_ui_sync import start as start_sync
+start_sync()
+# agent = window.navigator.userAgent.lower()
 
-agent = window.navigator.userAgent.lower()
-
-if("iphone" in agent or "android" in agent):
-    print("MOBILE")
-    from pygame_ui_sync import start as start_sync
-    start_sync()
-else:
-    from pygame_ui_async import start as start_async
-    start_async()
+# if("iphone" in agent or "android" in agent):
+#     print("MOBILE")
+#     from pygame_ui_sync import start as start_sync
+#     start_sync()
+# else:
+#     from pygame_ui_async import start as start_async
+#     start_async()
