f70ccd2a2c77729d1557d31c0b107a071457ceab
jtorbett23 <josh.torbett@outlook.com>
2025-09-01 01:13:32 +0100

    update local game config

--- pygame_ui.py
+++ pygame_ui.py
@@ -6,8 +6,6 @@ from pygame_ui_sync import start as start_sync
 agent = window.navigator.userAgent.lower()
 
 if("iphone" in agent or "android" in agent):
-    print("MOBILE")
     start_sync()
 else:
-    print("WEB")
     start_async()
