a0877e52d57bcf7e360046b15d7ace033272b586
jtorbett23 <josh.torbett@outlook.com>
2025-09-01 01:12:11 +0100

    update gh-pages

--- gh-pages.sh
+++ gh-pages.sh
@@ -6,7 +6,10 @@ git checkout -b gh-pages
 cp index.html ./ui/index.html
 cp pygame_core.py ./ui/pygame_core.py
 cp pygame_ui.py ./ui/pygame_ui.py
-sed -i "s|!URL|$GHPAGES_URL|g" ./ui/pygame_ui.py
+cp pygame_ui_sync.py ./ui/pygame_ui_sync.py
+cp pygame_ui_async.py ./ui/pygame_ui_async.py
+sed -i "s|!URL|$URL|g" ./ui/pygame_ui_sync.py
+sed -i "s|!URL|$URL|g" ./ui/pygame_ui_async.py
 sed -i "s|!URL|$GHPAGES_URL|g" ./ui/index.html
 rm -r ./*.py
 rm -r ./*.h5
