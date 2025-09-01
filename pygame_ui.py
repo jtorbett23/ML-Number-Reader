cfbc347b3172fec28358e668f141d30c6156d99c
jtorbett23 <josh.torbett@outlook.com>
2025-09-01 10:09:28 +0100

    remove prints

--- pygame_ui.py
+++ pygame_ui.py
@@ -3,7 +3,6 @@ start_sync()
 agent = window.navigator.userAgent.lower()
 
 if("iphone" in agent or "android" in agent):
-    print("MOBILE")
     from pygame_ui_sync import start as start_sync
     start_sync()
 else:
