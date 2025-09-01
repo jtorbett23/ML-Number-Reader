77e10c477ba3394fe67c83084d733d1b681d9111
jtorbett23 <josh.torbett@outlook.com>
2025-09-01 13:32:59 +0100

    flip condition to have sync as default

--- pygame_ui.py
+++ pygame_ui.py
@@ -1,8 +1,8 @@
 from pyscript import window
 
-if (window.navigator.maxTouchPoints > 1):
-    from pygame_ui_sync import start as start_sync
-    start_sync()
-else:
+if (window.navigator.maxTouchPoints <= 1):
     from pygame_ui_async import start as start_async
     start_async()
+else:
+    from pygame_ui_sync import start as start_sync
+    start_sync()
