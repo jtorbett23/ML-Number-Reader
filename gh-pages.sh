cd9ffbf98d558af423a639445edb9a26999ccc2b
jtorbett23 <56300912+jtorbett23@users.noreply.github.com>
2026-01-28 20:45:40 +0000

    update sed command for mac

--- gh-pages.sh
+++ gh-pages.sh
@@ -8,10 +8,10 @@ cp pygame_core.py ./ui/pygame_core.py
 cp pygame_ui.py ./ui/pygame_ui.py
 cp pygame_ui_sync.py ./ui/pygame_ui_sync.py
 cp pygame_ui_async.py ./ui/pygame_ui_async.py
-sed -i "s|!URL|$GHPAGES_URL|g" ./ui/pygame_ui.py
-sed -i "s|!URL|$GHPAGES_URL|g" ./ui/pygame_ui_sync.py
-sed -i "s|!URL|$GHPAGES_URL|g" ./ui/pygame_ui_async.py
-sed -i "s|!URL|$GHPAGES_URL|g" ./ui/index.html
+sed -i '' -e "s|!URL|$GHPAGES_URL|g" ./ui/pygame_ui.py
+sed -i '' -e "s|!URL|$GHPAGES_URL|g" ./ui/pygame_ui_sync.py
+sed -i '' -e "s|!URL|$GHPAGES_URL|g" ./ui/pygame_ui_async.py
+sed -i '' -e "s|!URL|$GHPAGES_URL|g" ./ui/index.html
 rm -r ./*.py
 rm -r ./*.h5
 rm -r ./*.md
