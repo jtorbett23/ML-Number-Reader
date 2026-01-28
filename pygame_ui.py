7ba3948ace09772661aa13a03ccb01563f3e60c2
jtorbett23 <56300912+jtorbett23@users.noreply.github.com>
2026-01-28 16:55:30 +0000

    add prints

--- pygame_ui.py
+++ pygame_ui.py
@@ -2,7 +2,9 @@ from pyscript import window
 
 if (window.navigator.maxTouchPoints <= 1):
     from pygame_ui_async import start as start_async
+    print("ASYNC")
     start_async()
 else:
     from pygame_ui_sync import start as start_sync
+    print("SYNC")
     start_sync()
